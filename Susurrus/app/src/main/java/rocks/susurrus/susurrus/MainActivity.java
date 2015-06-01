ca2e200e7ba2377e69e31b3314d1c5a6ad5a2943
deshi-basara <simon@i-was-perfect.net>
2015-06-01 19:50:53 +0200

    Try to discover available wifi direct services (blueprint)

--- Susurrus/app/src/main/java/rocks/susurrus/susurrus/MainActivity.java
+++ Susurrus/app/src/main/java/rocks/susurrus/susurrus/MainActivity.java
@@ -5,8 +5,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,14 +18,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import rocks.susurrus.susurrus.network.WiFiDirectBroadcastReceiver;
 
 
-public class MainActivity extends Activity {
-
-    private final String logTag = "MainActivity";
+public class MainActivity extends ActionBarActivity {
+    private static final String LOG_TAG = "MainActivity";
 
     private boolean firstStart = false;
+
+    /**
+     * Networking
+     */
+    // intent-filter for reacting on network changes
+    private final IntentFilter wifiIntentFilter = new IntentFilter();
+    private WifiP2pManager wifiManager;
+    private WifiP2pManager.Channel wifiChannel;
+    private WiFiDirectBroadcastReceiver wifiReceiver;
+
+    /**
+     * Views
+     */
+    private FloatingActionButton createButton;
+    private Button discoverButton;
+
+
+
+    final HashMap<String, String> buddies = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +59,20 @@ public class MainActivity extends Activity {
             Intent intentIntro = new Intent(this, IntroActivity.class);
             startActivity(intentIntro);
         }
-        else if(true) {
+        else if(false) {
             Intent intentChat = new Intent(this, ChatActivity.class);
             startActivity(intentChat);
         }
+
+        setView();
+        setWifi();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
     }
 
@@ -61,4 +90,162 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void setWifi() {
+        // set filter-actions, that will later be handled by the WiFiDirectBroadcastReceiver
+        wifiIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        wifiIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        wifiIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        wifiIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        // get an instance of the WifiP2PManager
+        wifiManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        // register application with the WifiP2PManager
+        wifiChannel = wifiManager.initialize(this, getMainLooper(), null);
+        // get an instance of the broadcast receiver and set needed data
+        wifiReceiver = WiFiDirectBroadcastReceiver.getInstance();
+        wifiReceiver.setWifiManager(wifiManager);
+        wifiReceiver.setWifiChannel(wifiChannel);
+        wifiReceiver.setActivity(this);
+    }
+
+    private void setView() {
+        // get needed views
+        createButton = (FloatingActionButton) findViewById(R.id.button_create);
+        discoverButton = (Button) findViewById(R.id.action_discover);
+
+        // set events
+        createButton.setOnClickListener(createButtonListener);
+    }
+
+    /**
+     * Is called whenever an ActionButton from the ActionBar is clicked.
+     * @param item Clicked MenuItem
+     * @return True, after handling
+     */
+    public boolean handleActionButton(MenuItem item) {
+        int actionButtonId = item.getItemId();
+
+        // which action button was clicked?
+        if(actionButtonId == R.id.action_discover) {
+            discoverRooms();
+        }
+
+        return true;
+    }
+
+    /**
+     * Starts the discovery of chat rooms.
+     */
+    private void discoverRooms() {
+        Log.d(LOG_TAG, "Start discovering rooms ...");
+
+        wifiManager.setDnsSdResponseListeners(wifiChannel, servListener, txtListener);
+
+        // get an instance of the WifiP2P service request object
+        WifiP2pDnsSdServiceRequest roomRequest = WifiP2pDnsSdServiceRequest.newInstance();
+
+        // add a service discovery request
+        wifiManager.addServiceRequest(wifiChannel, roomRequest,
+                new WifiP2pManager.ActionListener() {
+                @Override
+                public void onSuccess() {
+                    // Success!
+                    Log.d(LOG_TAG, "addServiceRequest success");
+                }
+
+                @Override
+                public void onFailure(int code) {
+                    // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+                    Log.d(LOG_TAG, "addServiceRequest error: " + code);
+                }
+            }
+        );
+
+        // make the request
+        wifiManager.discoverServices(wifiChannel, new WifiP2pManager.ActionListener() {
+
+                    @Override
+                    public void onSuccess() {
+                        // Success!
+                        Log.d(LOG_TAG, "Request made successfully");
+                    }
+
+                    @Override
+                    public void onFailure(int code) {
+                        // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+                        if (code == WifiP2pManager.P2P_UNSUPPORTED) {
+                            Log.d(LOG_TAG, "P2P isn't supported on this device.");
+                        }
+                    }
+                }
+        );
+    }
+
+    /**
+     * On click listener: createButton.
+     * Opens a new chat room creation activity.
+     */
+    private View.OnClickListener createButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // open a new chat creation intent
+            Intent intentChat = new Intent(getApplicationContext(), CreateActivity.class);
+            startActivity(intentChat);
+        }
+    };
+
+    /**
+     * On receive listener: wifiManager.
+     * Callback invocation when Bonjour TXT record is available for a room.
+     */
+    private WifiP2pManager.DnsSdTxtRecordListener txtListener =
+            new WifiP2pManager.DnsSdTxtRecordListener() {
+
+                @Override
+                /* Callback includes:
+                 * fullDomain: full domain name: e.g "printer._ipp._tcp.local."
+                 * record: TXT record dta as a map of key/value pairs.
+                 * device: The device running the advertised service.
+                 */
+                public void onDnsSdTxtRecordAvailable(String fullDomainName, Map<String,
+                        String> txtRecordMap, WifiP2pDevice srcDevice) {
+
+                        Log.d(LOG_TAG, "DnsSdTxtRecord available -" + txtRecordMap.toString());
+                        buddies.put(srcDevice.deviceAddress, txtRecordMap.get("buddyname"));
+                }
+    };
+
+    /**
+     * On receive listener: wifiManager.
+     * Receives the actual description and connection information of a room.
+     * The previous code snippet implemented a Map object to pair a device address with the buddy name. The service response listener uses this to link the DNS record with the corresponding service information
+     */
+    private WifiP2pManager.DnsSdServiceResponseListener servListener = new WifiP2pManager
+            .DnsSdServiceResponseListener() {
+        @Override
+        public void onDnsSdServiceAvailable(String instanceName, String registrationType,
+                                            WifiP2pDevice resourceType) {
+
+            // Update the device name with the human-friendly version from
+            // the DnsTxtRecord, assuming one arrived.
+            resourceType.deviceName = buddies
+                    .containsKey(resourceType.deviceAddress) ? buddies
+                    .get(resourceType.deviceAddress) : resourceType.deviceName;
+
+            // Add to the custom adapter defined specifically for showing
+            // wifi devices.
+            /*WiFiDirectServicesList fragment = (WiFiDirectServicesList) getFragmentManager()
+                    .findFragmentById(R.id.frag_peerlist);
+            WiFiDevicesAdapter adapter = ((WiFiDevicesAdapter) fragment
+                    .getListAdapter());
+
+            adapter.add(resourceType);
+            adapter.notifyDataSetChanged();*/
+            Log.d(LOG_TAG, "onBonjourServiceAvailable " + instanceName);
+        }
+    };
+
+
+
 }
--- Susurrus/app/src/main/java/rocks/susurrus/susurrus/network/WiFiDirectBroadcastReceiver.java
+++ Susurrus/app/src/main/java/rocks/susurrus/susurrus/network/WiFiDirectBroadcastReceiver.java
@@ -1,44 +1,59 @@
 package rocks.susurrus.susurrus.network;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import rocks.susurrus.susurrus.ChatActivity;
 
 /**
  * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
+ * Allow only one instance of the class by using the Singleton pattern.
  */
 public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
-
     private static final String LOG_TAG = "WifiBroadcastReceiver";
 
-    private WifiP2pManager mManager;
-    private WifiP2pManager.Channel mChannel;
-    private ChatActivity mActivity;
+    // singleton instance
+    private static WiFiDirectBroadcastReceiver singleInstance;
+
+
+    private WifiP2pManager wifiManager;
+    private WifiP2pManager.Channel wifiChannel;
+    private Activity baseActivity;
 
     private List peers = new ArrayList();
 
     /**
-     * Constructor.
-     * Sets
-     * @param manager
-     * @param channel
-     * @param activity
+     * Class constructor.
      */
-    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
-                                       ChatActivity activity) {
+    protected WiFiDirectBroadcastReceiver() {
         super();
-        this.mManager = manager;
-        this.mChannel = channel;
-        this.mActivity = activity;
+    }
+
+    /**
+     * Maintains a static reference to the lone singleton instance and returns the reference from.
+     * @return WifiDirectBroadcastReceiver instance
+     */
+    public static WiFiDirectBroadcastReceiver getInstance() {
+        // is there already an instance of the class?
+        if(singleInstance == null) {
+            // no instance, create one
+            singleInstance = new WiFiDirectBroadcastReceiver();
+        }
+
+        return singleInstance;
     }
 
     @Override
@@ -63,9 +78,9 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
             // request available peers from the wifi p2p manager. This is an
             // asynchronous call and the calling activity is notified with a
             // callback on PeerListListener.onPeersAvailable()
-            if(mManager != null) {
+            if(this.wifiManager != null) {
                 Log.d(LOG_TAG, "mManager requesting peers ...");
-                mManager.requestPeers(mChannel, peerListListener);
+                this.wifiManager.requestPeers(this.wifiChannel, peerListListener);
             }
             else {
                 Log.d(LOG_TAG, "mManager is empty");
@@ -74,7 +89,21 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
         // Respond to new connection or disconnections
         else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
-            //Log.d(LOG_TAG, "WIFI_P2P_CONNECTION_CHANGED_ACTION");
+            Log.d(LOG_TAG, "WIFI_P2P_CONNECTION_CHANGED_ACTION");
+
+            // broadcast received before having a valid wifiManager instance?
+            if(this.wifiManager == null) {
+                return;
+            }
+            else {
+                // get network information
+                NetworkInfo networkInfo = (NetworkInfo) intent
+                        .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+
+            }
+
+
         }
 
         // Respond to this device's wifi state changing
@@ -104,6 +133,47 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
             return false;
         }
     }
+
+    public void createNewRoom(Map roomData) {
+        // Service information. Pass it an instance name, service type
+        // _protocol._transportlayer, and the map containing
+        // information other devices will want once they connect to this one.
+        WifiP2pDnsSdServiceInfo roomServiceInfo =
+                WifiP2pDnsSdServiceInfo.newInstance("_test", "_presence._tcp", roomData);
+
+        // Add the local service, sending the service info, network channel,
+        // and listener that will be used to indicate success or failure of
+        // the request.
+        wifiManager.addLocalService(wifiChannel, roomServiceInfo, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                // Command successful! Code isn't necessarily needed here,
+                // Unless you want to update the UI or add logging statements.
+                Log.d(LOG_TAG, "Room created");
+            }
+
+            @Override
+            public void onFailure(int arg0) {
+                // Command failed.  Check for P2P_UNSUPPORTED, ERROR, or BUSY
+                Log.d(LOG_TAG, "Error code: " + arg0);
+            }
+        });
+    }
+
+    /**
+     * Setter methods.
+     */
+    public void setWifiManager(WifiP2pManager manager) {
+        this.wifiManager = manager;
+    }
+    public void setWifiChannel(WifiP2pManager.Channel channel) {
+        this.wifiChannel = channel;
+    }
+    public void setActivity(Activity activity) {
+        this.baseActivity = activity;
+    }
+
+
 
     /**
      * Custom listener called by mManager.requestPeers(...).
